99052c37ea755e8c3df09cd442bfbe4d2d71a0ea
Protengstar <musthofasifaulq@gmail.com>
2024-02-01 13:22:02 +0700

    git remote

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git remote2</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptates ea beatae deleniti obcaecati nam sit possimus maxime. Dicta molestiae suscipit incidunt odit. Corrupti molestias aperiam voluptatibus dicta pariatur numquam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda voluptates ea beatae deleniti obcaecati nam sit possimus maxime. Dicta molestiae suscipit incidunt odit. Corrupti molestias aperiam voluptatibus dicta pariatur numquam!</p>
+</body>
+</html>
